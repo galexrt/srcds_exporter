3a2f385165fba191bdd17d0d2ae06f6367b74a0a
Alexander Trost <galexrt@googlemail.com>
2017-04-24 08:56:33 +0200

    Added tests for parseHostname, parseMap, parseVersion, parsePlayerCount

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,6 +7,89 @@ import (
 	"github.com/galexrt/srcds_exporter/models"
 )
 
+var parseHostnameTests = []struct {
+	request  string
+	expected string
+}{
+	{
+		`hostname: Example server`,
+		"Example server",
+	},
+	{
+		`hostname: [TEST] ÜÄÖÜ server`,
+		"[TEST] ÜÄÖÜ server",
+	},
+}
+
+func TestParseHostname(t *testing.T) {
+	for _, tt := range parseHostnameTests {
+		actual := parseHostname(tt.request)
+		if actual != tt.expected {
+			t.Errorf("Parse(%s): expected %s, actual %s", tt.request, tt.expected, actual)
+		}
+	}
+}
+
+var parseVersionTests = []struct {
+	request  string
+	expected string
+}{
+	{
+		`version : 16.12.01/24 6729 secure`,
+		"16.12.01/24 6729 secure",
+	},
+}
+
+func TestParseVersion(t *testing.T) {
+	for _, tt := range parseVersionTests {
+		actual := parseVersion(tt.request)
+		if actual != tt.expected {
+			t.Errorf("Parse(%s): expected %s, actual %s", tt.request, tt.expected, actual)
+		}
+	}
+}
+
+var parseMapTests = []struct {
+	request  string
+	expected string
+}{
+	{
+		`map     : rp_retribution_v2 at: 0 x, 0 y, 0 z`,
+		"rp_retribution_v2",
+	},
+}
+
+func TestParseMap(t *testing.T) {
+	for _, tt := range parseMapTests {
+		actual := parseMap(tt.request)
+		if actual != tt.expected {
+			t.Errorf("Parse(%s): expected %s, actual %s", tt.request, tt.expected, actual)
+		}
+	}
+}
+
+var parsePlayerCountTests = []struct {
+	request  string
+	expected models.PlayerCount
+}{
+	{
+		`players : 1 (64 max)`,
+		*&models.PlayerCount{
+			Current: 1,
+			Max:     64,
+		},
+	},
+}
+
+func TestParsePlayerCount(t *testing.T) {
+	for _, tt := range parsePlayerCountTests {
+		actual := *parsePlayerCount(tt.request)
+		if !reflect.DeepEqual(actual, tt.expected) {
+			t.Errorf("Parse(%s): expected %v, actual %v", tt.request, tt.expected, actual)
+		}
+	}
+}
+
 var parserTests = []struct {
 	name     string
 	request  string
